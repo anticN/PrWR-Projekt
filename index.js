acdbc9e53755f34a994e720fa62e7dd6bc2868a1
anticN <n.antic@virine.com>
2022-12-09 17:08:18 +0100

    add: searchbar

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Create a search bar and add it to ./index.html
-const searchBar = document.getElementById('searchBar');
+/*const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
     const filteredCharacters = characters.filter((character) => {
@@ -17,4 +17,53 @@ searchBar.addEventListener('keyup', (e) => {
     height:43,
     top:0,
 });
+*/
+ 
+/*
+Testversuch von popups für schwierige Begriffe 
+*/
+/*
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+*/
+
+/*
+fix Navbar
+*/
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+/*window.onscroll = function() {myFunction()};
+
+          var navbar = document.getElementById("navbar");
+          var sticky = navbar.offsetTop;
+
+          function myFunction() {
+            if (window.pageYOffset >= sticky) {
+              navbar.classList.add("sticky")
+            } else {
+              navbar.classList.remove("sticky");
+            }
+          }
 */
---
+++ search.js
@@ -0,0 +1,12 @@
+/*const p = document.getElementById("p");
+
+function search() {
+
+    let input = document.getElementById("input").value;
+
+    if (input !== "") {
+        let regExp = new RegExp(input, "gi");
+        p.innerHtml = (p.textContent).replace(regExp, "<mark>$&</mark>");
+    }
+}   
+*/
